34c12427f4aa5792ccd58191fb61ede480c0f8fc
MarinJ <mjuric00@fesb.hr>
2020-12-25 21:33:53 +0100

    Start experimenting with the lazy APIs and constraintLayout more. Add basic skeleton for the todo list

--- app/src/main/java/com/example/crossingschedule/presentation/home/HomePage.kt
+++ app/src/main/java/com/example/crossingschedule/presentation/home/HomePage.kt
@@ -2,6 +2,7 @@ package com.example.crossingschedule.presentation.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.Dimension
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.crossingschedule.R
+import com.example.crossingschedule.domain.model.CrossingTodo
 import com.example.crossingschedule.presentation.home.components.BackgroundImage
+import com.example.crossingschedule.presentation.home.components.CrossingTodoList
 import com.example.crossingschedule.presentation.home.components.RawIngredientRow
 import com.example.crossingschedule.ui.crossingTypography
 
@@ -23,51 +26,65 @@ fun HomePage() {
             BackgroundImage(resourceId = R.drawable.home_background)
             ConstraintLayout {
                 val (checkListText, dateSelector,
-                    todoList, notes,
-                    rawIngredientRow, shopContainer,
-                    turnipPriceList, villagerList) = createRefs()
+                        todoList, notes,
+                        rawIngredientRow, shopContainer,
+                        turnipPriceList, villagerList) = createRefs()
 
                 Card(
-                    modifier = Modifier
-                        .padding(vertical = 16.dp)
-                        .constrainAs(checkListText) {
-                            top.linkTo(parent.top)
-                            start.linkTo(parent.start)
-                        },
-                    elevation = 4.dp,
-                    shape = RoundedCornerShape(topRight = 16.dp, bottomRight = 16.dp),
+                        modifier = Modifier
+                                .padding(vertical = 16.dp)
+                                .constrainAs(checkListText) {
+                                    top.linkTo(parent.top)
+                                    start.linkTo(parent.start)
+                                },
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(topRight = 16.dp, bottomRight = 16.dp),
                 ) {
                     Text(
-                        modifier = Modifier
-                            .padding(start = 24.dp, top = 8.dp, bottom = 8.dp, end = 16.dp),
-                        text = "Daily Checklist",
-                        style = crossingTypography.h2,
+                            modifier = Modifier
+                                    .padding(start = 24.dp, top = 8.dp, bottom = 8.dp, end = 16.dp),
+                            text = "Daily Checklist",
+                            style = crossingTypography.h2,
                     )
                 }
                 Card(
-                    modifier = Modifier
-                        .padding(vertical = 16.dp)
-                        .constrainAs(dateSelector) {
-                            top.linkTo(parent.top)
-                            start.linkTo(checkListText.end, margin = 24.dp)
-                            end.linkTo(parent.end)
-                        },
-                    elevation = 4.dp,
-                    shape = RoundedCornerShape(16.dp),
+                        modifier = Modifier
+                                .padding(vertical = 16.dp)
+                                .constrainAs(dateSelector) {
+                                    top.linkTo(parent.top)
+                                    start.linkTo(checkListText.end, margin = 24.dp)
+                                    end.linkTo(parent.end)
+                                },
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(16.dp),
                 ) {
                     Text(
-                        modifier = Modifier
-                            .padding(start = 8.dp, top = 4.dp, bottom = 4.dp, end = 16.dp),
-                        text = "Date: 18.12.2020",
-                        style = crossingTypography.h6.copy(fontWeight = FontWeight.Bold),
+                            modifier = Modifier
+                                    .padding(start = 8.dp, top = 4.dp, bottom = 4.dp, end = 16.dp),
+                            text = "Date: 18.12.2020",
+                            style = crossingTypography.h6.copy(fontWeight = FontWeight.Bold),
                     )
                 }
                 RawIngredientRow(Modifier
-                    .constrainAs(rawIngredientRow) {
-                        start.linkTo(checkListText.end, margin = 8.dp)
-                        top.linkTo(dateSelector.bottom)
-                        end.linkTo(parent.end, margin = 8.dp)
-                    }
+                        .constrainAs(rawIngredientRow) {
+                            start.linkTo(checkListText.end, margin = 8.dp)
+                            top.linkTo(dateSelector.bottom)
+                            end.linkTo(parent.end, margin = 8.dp)
+                        }
+                )
+                CrossingTodoList(Modifier
+                        .constrainAs(todoList) {
+                            width = Dimension.fillToConstraints
+                            start.linkTo(checkListText.start, margin = 16.dp)
+                            end.linkTo(checkListText.end)
+                            top.linkTo(checkListText.bottom, margin = 16.dp)
+                        },
+                        listOf(
+                                CrossingTodo("Prvi", false),
+                                CrossingTodo("Drugi", true),
+                                CrossingTodo("Treci", false),
+                                CrossingTodo("Cevrti", true),
+                        )//TODO REMOVE HARD CODED DATA
                 )
             }
         }
--- app/src/main/java/com/example/crossingschedule/presentation/home/components/HomeComponents.kt
+++ app/src/main/java/com/example/crossingschedule/presentation/home/components/HomeComponents.kt
@@ -2,11 +2,12 @@ package com.example.crossingschedule.presentation.home.components
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.imageFromResource
@@ -14,21 +15,22 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.unit.dp
 import com.example.crossingschedule.R
+import com.example.crossingschedule.domain.model.CrossingTodo
 
 @Composable
 fun BackgroundImage(
-    @DrawableRes resourceId: Int,
-    modifier: Modifier = Modifier,
+        @DrawableRes resourceId: Int,
+        modifier: Modifier = Modifier,
 ) {
     Image(
-        modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-        contentScale = ContentScale.FillBounds,
-        bitmap = imageFromResource(
-            AmbientContext.current.resources,
-            resourceId
-        )
+            modifier = modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+            contentScale = ContentScale.FillBounds,
+            bitmap = imageFromResource(
+                    AmbientContext.current.resources,
+                    resourceId
+            )
     )
 }
 
@@ -36,49 +38,73 @@ fun BackgroundImage(
 fun RawIngredientRow(modifier: Modifier = Modifier) {
     Row(modifier = modifier) {
         Image(
-            modifier = Modifier
-                .size(42.dp)
-                .padding(top = 4.dp),
-            bitmap = imageFromResource(
-                AmbientContext.current.resources,
-                R.drawable.tree_branch
-            )
+                modifier = Modifier
+                        .size(42.dp)
+                        .padding(top = 4.dp),
+                bitmap = imageFromResource(
+                        AmbientContext.current.resources,
+                        R.drawable.tree_branch
+                )
         )
         Image(
-            modifier = Modifier
-                .size(42.dp)
-                .padding(bottom = 4.dp),
-            bitmap = imageFromResource(
-                AmbientContext.current.resources,
-                R.drawable.stone
-            )
+                modifier = Modifier
+                        .size(42.dp)
+                        .padding(bottom = 4.dp),
+                bitmap = imageFromResource(
+                        AmbientContext.current.resources,
+                        R.drawable.stone
+                )
         )
         Image(
-            modifier = Modifier
-                .size(42.dp)
-                .padding(top = 4.dp),
-            bitmap = imageFromResource(
-                AmbientContext.current.resources,
-                R.drawable.clay
-            )
+                modifier = Modifier
+                        .size(42.dp)
+                        .padding(top = 4.dp),
+                bitmap = imageFromResource(
+                        AmbientContext.current.resources,
+                        R.drawable.clay
+                )
         )
         Image(
-            modifier = Modifier
-                .size(42.dp)
-                .padding(bottom = 4.dp),
-            bitmap = imageFromResource(
-                AmbientContext.current.resources,
-                R.drawable.hard_wood
-            )
+                modifier = Modifier
+                        .size(42.dp)
+                        .padding(bottom = 4.dp),
+                bitmap = imageFromResource(
+                        AmbientContext.current.resources,
+                        R.drawable.hard_wood
+                )
         )
         Image(
-            modifier = Modifier
-                .size(42.dp)
-                .padding(top = 4.dp),
-            bitmap = imageFromResource(
-                AmbientContext.current.resources,
-                R.drawable.iron_nugget
-            )
+                modifier = Modifier
+                        .size(42.dp)
+                        .padding(top = 4.dp),
+                bitmap = imageFromResource(
+                        AmbientContext.current.resources,
+                        R.drawable.iron_nugget
+                )
+        )
+    }
+}
+
+@Composable
+fun CrossingTodoList(
+        modifier: Modifier = Modifier,
+        todos: List<CrossingTodo>
+) {
+    Card(
+            shape = RoundedCornerShape(16.dp),
+            modifier = modifier
+    ) {
+        LazyColumn(
+                content = {
+                    items(todos) { todo ->
+                        Row(modifier = Modifier
+                                .padding(4.dp)) {
+                            Checkbox(checked = todo.isDone, onCheckedChange = { })
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = todo.message)
+                        }
+                    }
+                }
         )
     }
 }
