a95da5ec038041941a73cf1f39c0d4ae50edcc14
Marin Juričev <marin.juricev@undabot.com>
2021-02-06 00:41:01 +0100

    Finish styling the top part of login screen

--- app/src/main/java/com/example/crossingschedule/feature/login/presentation/LoginPage.kt
+++ app/src/main/java/com/example/crossingschedule/feature/login/presentation/LoginPage.kt
@@ -1,17 +1,19 @@
 package com.example.crossingschedule.feature.login.presentation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.imageFromResource
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.loadVectorResource
 import androidx.compose.ui.unit.dp
 import com.example.crossingschedule.R
 import com.example.crossingschedule.feature.login.presentation.components.LoginTab
@@ -27,24 +29,40 @@ fun LoginPage(
 ) {
     val selectedTabPosition = mutableStateOf(LOGIN_TAB_POSITION)
 
-    Scaffold {
+    Scaffold() {
         Column {
-            Row() {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 LoginTab(
                     selectedTabPosition = selectedTabPosition.value,
                     onTabClick = { selectedTabPosition.value = it }
                 )
-                Image(
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(24.dp),
-                    contentScale = ContentScale.FillBounds,
-                    bitmap = imageFromResource(
-                        AmbientContext.current.resources,
-                        R.drawable.daisy_mae
+                val image = loadVectorResource(id = R.drawable.person)
+                //loadVectorResource will load the vector image asynchronous
+                image.resource.resource?.let {
+                    Image(
+                        modifier = Modifier
+                            .padding(top = 16.dp, end = 16.dp)
+                            .clip(MaterialTheme.shapes.medium)
+                            .size(36.dp)
+                            .background(MaterialTheme.colors.primary)
+                            .border(
+                                width = 1.dp,
+                                color = MaterialTheme.colors.secondary,
+                                shape = MaterialTheme.shapes.medium
+                            )
+                            .shadow(
+                                elevation = 4.dp,
+                                shape = MaterialTheme.shapes.medium,
+                                clip = true
+                            ),
+                        imageVector = it,
+                        alignment = Alignment.BottomEnd,
+                        contentDescription = null
                     )
-                )
-
+                }
             }
             when (selectedTabPosition.value) {
                 LOGIN_TAB_POSITION -> Text("RENDER THE ACTUAL LOGIN FLOW")
