2841594926dcd38ef79493795bef3169e57a85ac
MarinJ <mjuric00@fesb.hr>
2021-03-31 00:22:25 +0200

    Add missing moshi annotation

--- app/src/main/java/com/example/crossingschedule/feature/auth/data/model/CreateAccountBody.kt
+++ app/src/main/java/com/example/crossingschedule/feature/auth/data/model/CreateAccountBody.kt
@@ -1,5 +1,8 @@
 package com.example.crossingschedule.feature.auth.data.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CreateAccountBody(
     val username: String
 )
