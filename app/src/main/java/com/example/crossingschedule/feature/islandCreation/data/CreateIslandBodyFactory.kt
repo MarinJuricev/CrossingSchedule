8147b13c16dc76a65667e173d8306d71c23b2e85
MarinJ <mjuric00@fesb.hr>
2021-04-25 00:49:01 +0200

    Add missing @Inject constructor to CreateIslandBodyFactory.kt

--- app/src/main/java/com/example/crossingschedule/feature/islandCreation/data/CreateIslandBodyFactory.kt
+++ app/src/main/java/com/example/crossingschedule/feature/islandCreation/data/CreateIslandBodyFactory.kt
@@ -2,8 +2,9 @@ package com.example.crossingschedule.feature.islandCreation.data
 
 import com.example.crossingschedule.feature.islandCreation.data.model.CreateIslandBody
 import com.example.crossingschedule.feature.islandSelection.domain.model.Hemisphere
+import javax.inject.Inject
 
-class CreateIslandBodyFactory {
+class CreateIslandBodyFactory @Inject constructor() {
 
     fun create(
         islandName: String,
